de699d9d6395ff5acf9d06c8eb221afa844ade4a
Shubham Chopra <shubhamchopra553@gmail.com>
2018-02-15 17:01:48 +0530

    Create TRICOUNT.cpp

---
+++ TRICOUNT.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	unsigned long long int t;
+	cin>>t;
+	while(t--)
+	{
+	   unsigned long long int n;
+	    cin>>n;
+	   unsigned long long int ans;
+	    ans=(n*(n+2)*(2*n+1)/8);
+	cout<<ans<<endl;
+	    
+	}
+	return 0;
+
+}
