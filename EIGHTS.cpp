fa2c4cfb649359f6d2686404ebb6340b52e7931c
Shubham Chopra <shubhamchopra553@gmail.com>
2018-04-26 16:42:50 +0530

    Create EIGHTS.cpp

---
+++ EIGHTS.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+long long int t;
+cin>>t;
+while(t--)
+{
+	long long int n;
+		cin>>n;
+		long long int ans;
+		ans=192+(n-1)*250;
+		cout<<ans<<endl;
+	
+}
+	return 0;
+}
