241072423dcf80e0bdc8b0ef8555604ba5da3666
Shubham Chopra <shubhamchopra553@gmail.com>
2018-04-26 16:37:16 +0530

    Create HUBULLU.cpp

---
+++ HUBULLU.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int t;
+    cin>>t;
+   
+    while(t--)
+    {
+        
+        long long int n,a;
+        cin>>n>>a;
+        if(a==0)
+        {
+            cout<<"Airborne wins."<<endl;
+        }
+        else if(a==1)
+        {
+            cout<<"Pagfloyd wins."<<endl;
+        }
+    }
+    
+    
+    
+    
+return 0;    
+}
